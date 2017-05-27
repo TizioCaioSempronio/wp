790c277b048abb9ae1d23129a03e70595e31e1cc
TizioCaioSempronio <holly.era77@gmail.com>
2017-05-27 17:45:25 +0200

    Add files via upload

---
+++ wp-content/themes/tema wp/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	
+	<?php wp_head(); ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+	<title>IsoladelTurismo</title>
+</head>
+<body>
+	
+	<div class="jumbotron">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#"></a>
+
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+							<li><a href="#">Chi siamo</a></li>
+							<li><a href="#contact">Contattaci</a></li>
+							
+						</ul>
+						<form class="navbar-form navbar-right">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Cerca">
+							</div>
+							<button type="submit" class="btn btn-default">Invia</button>
+						</form>		
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</div>
+	</div>
+</div><!-- /.container-fluid -->
+</nav>      
--- wp-content/themes/tema wp/index.php
+++ wp-content/themes/tema wp/index.php
@@ -1,1 +1,57 @@
-<?php ?>
+<?php get_header(); ?>
+
+<div class="row">
+<div class="col-md-12 welcome">
+	<h1>IsoladelTurismo</h1>
+	<h4>Il sito del turismo in Sardegna</h4>
+</div>
+</div>      </div>
+</div>
+<div class="row" id="contact">
+	<div class="col-lg-12 text-center">
+		<h2>Contattaci!</h2>
+		<h3>Per favore, inserisci le informazioni richieste nei riquadri sottostanti</h3>
+	</div>
+</div>
+<form>
+	<div class="row row-form">
+		<div class="col-lg-12">
+			<div class="col-lg-6">
+				<div class="form-group">
+					<label>Nome e Cognome</label>
+					<div class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+						<input name="name" type="text" class="form-control input-lg" placeholder="Inserisci nome e cognome" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label>Email</label>
+					<div class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+						<input name="email" type="email" class="form-control input-lg" placeholder="Email" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label>Telefono</label>
+					<div class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+						<input name="phone" type="number" class="form-control input-lg" placeholder="Inserisci il numero di telefono" required>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="form-group">
+					<label>Commento</label>
+					<textarea spellcheck class="form-control input-lg" placeholder="Per favore, inserisci un commento"></textarea>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<button type="submit" class="btn btn-primary btn-lg">Invia</button>
+			</div>
+		</div>
+	</div>
+
+
+<?php get_footer(); ?>
