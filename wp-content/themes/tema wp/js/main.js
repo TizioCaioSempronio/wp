d5e5fd58a3931e02fedfb0459de1a10e847b8a6e
TizioCaioSempronio <holly.era77@gmail.com>
2017-06-05 10:33:46 +0200

    Add files via upload

--- wp-content/themes/tema wp/js/main.js
+++ wp-content/themes/tema wp/js/main.js
@@ -1,26 +1,16 @@
 (function ($){
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
 })(jQuery);
